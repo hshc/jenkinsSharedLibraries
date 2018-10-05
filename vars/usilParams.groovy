7377df0e8d57ebec2bfc9a61fe08111331dab356
smurer <smurer@si2m.fr>
2018-10-05 09:06:26 +0000

    usilParams.groovy edited online with Bitbucket

--- vars/usilParams.groovy
+++ vars/usilParams.groovy
@@ -1,4 +1,4 @@
-def call() {
+class params {
 	def trigrammeAppli
 	def codeAppli
 	def projectName
@@ -6,7 +6,7 @@ def call() {
 	def gitBranchName
 	def gitTag
 	def dockerRegistryRepoAppli
-	def dockerRegistryUrl='https://dtr.docker.si2m.tec'
+	def dockerRegistryUrl = 'https://dtr.docker.si2m.tec'
 	def dockerRegistryImageBuild='test-store/si2m-npm10-build'
 	def dockerRegistryRepo='repo-store'
 	def dockerRegistryUser='7c1.03ee-4888-4843-951f-2f7b4f846b8c'
