892642b8e128f8c0d23e8d91f8dddddf22efa0ce
smurer <smurer@si2m.fr>
2018-10-05 14:40:53 +0000

    usilBuildPushDockerRegistry.groovy edited online with Bitbucket

--- vars/usilBuildPushDockerRegistry.groovy
+++ vars/usilBuildPushDockerRegistry.groovy
@@ -1,5 +1,6 @@
 def call(def dockerRegistryUrl,def dockerRegistryUser, def dockerRegistryRepoAppli){
     stage('Build Docker & push DTR'){
+      echo dockerRegistryUser
       withCredentials([usernamePassword( credentialsId: dockerRegistryUser , usernameVariable: 'userName', passwordVariable: 'password')]) {
         docker.withRegistry(dockerRegistryUrl) {
 			sh "docker login -u ${userName} -p ${password} ${dockerRegistryUrl}"
