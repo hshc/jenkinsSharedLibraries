b95978786751cca9829d6f2b30df8df00a403668
smurer <smurer@si2m.fr>
2018-10-09 16:00:23 +0000

    usilProjectCheckout.groovy edited online with Bitbucket

--- vars/usilProjectCheckout.groovy
+++ vars/usilProjectCheckout.groovy
@@ -10,12 +10,12 @@ def call() {
 			userRemoteConfigs: scm.userRemoteConfigs,
 			])
 		def gitUrl = sh(returnStdout: true, script: 'git config remote.origin.url').trim()
-		gitProjectName = sh(returnStdout: true, script: "echo '$gitUrl' | awk -F'/si2m/' '{print \$2}' |  awk -F'.git' '{print \$1}'").trim()
-		trigrammeAppli = sh(returnStdout: true, script: "echo '$gitProjectName' | awk -F'_' '{print \$1}'").trim()
-		codeAppli = sh(returnStdout: true, script: "echo '$gitProjectName' | awk -F'_' '{print \$2}'").trim()
-		projectName = sh(returnStdout: true, script: "echo '$gitProjectName' | awk -F'_' '{print \$3}'").trim()
+		usilParams.gitProjectName = sh(returnStdout: true, script: "echo '$gitUrl' | awk -F'/si2m/' '{print \$2}' |  awk -F'.git' '{print \$1}'").trim()
+		usilParams.trigrammeAppli = sh(returnStdout: true, script: "echo '$gitProjectName' | awk -F'_' '{print \$1}'").trim()
+		usilParams.codeAppli = sh(returnStdout: true, script: "echo '$gitProjectName' | awk -F'_' '{print \$2}'").trim()
+		usilParams.projectName = sh(returnStdout: true, script: "echo '$gitProjectName' | awk -F'_' '{print \$3}'").trim()
 		gitTag = sh(returnStdout: true, script: "git tag -l | sort -V | tail -n 1 | cut -c 2-").trim()
 		echo env.BRANCH_NAME
-		gitBranchName=env.BRANCH_NAME
+		usilParams.gitBranchName=env.BRANCH_NAME
 	}
 }
