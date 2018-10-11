8609c85959d6410f6431d33102ced0ecc5e1db6c
smurer <smurer@si2m.fr>
2018-10-11 09:59:54 +0000

    usilSwarmDeploy.groovy edited online with Bitbucket

--- vars/usilSwarmDeploy.groovy
+++ vars/usilSwarmDeploy.groovy
@@ -17,10 +17,10 @@ def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
 		echo "DOCKER_CERT_PATH=${dockerCertPath}"
 		echo "DOCKER_HOST=${dockerUcp}"
 		
-		 withEnv(['DOCKER_TLS_VERIFY=1',"DOCKER_CERT_PATH=${dockerCertPath}","DOCKER_HOST=${dockerUcp}"])
-		{
+		// withEnv(['DOCKER_TLS_VERIFY=1',"DOCKER_CERT_PATH=${dockerCertPath}","DOCKER_HOST=${dockerUcp}"])
+	    //	{
 			//sh "export DTRIMAGE=${dockerRegistryRepoAppli} && cd ${codeEnv} && docker-compose config > docker-compose-deploy.yml"
 			//sh "docker stack deploy --prune --compose-file=${codeEnv}/docker-compose-deploy.yml ${gitProjectName}"
-		}
+	    //	}
     }
 }
