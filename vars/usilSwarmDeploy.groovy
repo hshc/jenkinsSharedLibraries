addd6ca68fa5254c78dd504e183a8ce136607374
smurer <smurer@si2m.fr>
2018-10-11 08:52:53 +0000

    usilSwarmDeploy.groovy edited online with Bitbucket

--- vars/usilSwarmDeploy.groovy
+++ vars/usilSwarmDeploy.groovy
@@ -2,23 +2,20 @@ def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
 	// gestion des certificats pour connexion UCP
 	if (codeEnv == 'e0') {
 		def dockerUcp='tcp://ucp.docker.si2m.tec:443'
-		def dockerCertPath="$JENKINS_HOME/docker_ucp_prod"
+		def dockerCertPath="${JENKINS_HOME}/docker_ucp_prod"
 		}
 	else if (codeEnv == 'e1') {
 		def dockerUcp='tcp://ucp.pprod.docker.si2m.tec:443'
-		def dockerCertPath="$JENKINS_HOME/docker_ucp_pprod"
+		def dockerCertPath="${JENKINS_HOME}/docker_ucp_pprod/"
 		}
 	else {
-	    echo 'test'
-		echo codeEnv
 		def dockerUcp='tcp://ucp.recf.docker.si2m.tec:443'
-		def dockerCertPath="${JENKINS_HOME}/docker_ucp_recf"
-		echo dockerCertPath
+		def dockerCertPath="${JENKINS_HOME}/docker_ucp_recf/"
 	}
 	echo 'dockerCertPath'
 	echo dockerCertPath
     stage ("Deploiement UCP Docker ${codeEnv}") {
-		withEnv(['DOCKER_TLS_VERIFY=1',"DOCKER_CERT_PATH=${dockerCertPath}","DOCKER_HOST=${dockerCertPath}"]) {
+		withEnv(['DOCKER_TLS_VERIFY=1',"DOCKER_CERT_PATH=${dockerCertPath}","DOCKER_HOST=${dockerUcp}"]) {
 			sh "export DTRIMAGE=${dockerRegistryRepoAppli} && cd ${codeEnv} && docker-compose config > docker-compose-deploy.yml"
 			sh "docker stack deploy --prune --compose-file=${codeEnv}/docker-compose-deploy.yml ${gitProjectName}"
 		}
