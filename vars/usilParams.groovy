13dbf8035d06b05c8bc915f02755d30681dad86b
smurer <smurer@si2m.fr>
2018-10-12 07:18:34 +0000

    usilParams.groovy edited online with Bitbucket

--- vars/usilParams.groovy
+++ vars/usilParams.groovy
@@ -2,6 +2,7 @@ class usilParams {
 	def dockerRegistryUrl = 'https://dtr.docker.si2m.tec'
 	def dockerRegistryImageBuild='test-store/si2m-npm10-build'
 	def dockerRegistryRepo='repo-store'
+	def dockerRegistryRepoProd='app-store'
 	def dockerRegistryUser='DockerDTR'
 	def nexusRepo='http://vlc3inf013:8081/repository/npm-si2m'
 	def gitBitbucketUrl='https://bitbucket.org/si2m/'
