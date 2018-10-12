ea81185512dd5613f9220863b6d80c069d63e26c
smurer <smurer@si2m.fr>
2018-10-12 12:18:54 +0000

    usilSwarmDeployE2e.groovy edited online with Bitbucket

--- vars/usilSwarmDeployE2e.groovy
+++ vars/usilSwarmDeployE2e.groovy
@@ -10,12 +10,13 @@ def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
         echo urlRecf
         def prefixeUrl = sh(returnStdout: true, script: "echo '$urlRecf' | awk -F'http://' '{print \$2}' |  awk -F'.int.c-cloud' '{print \$1}'").trim()
         def urlE2e = "${prefixeUrl}-e2e.recf.cloud.si2m.tec"
+        def amap=['URL'='"${urlE2e}"']
+        writeYaml file: "${codeEnv}/.env", data :amap
         
-        
-    stage ('Deploiement UCP Docker ${codeEnv}') {
-		withEnv(['DOCKER_TLS_VERIFY=1',"DOCKER_CERT_PATH=$dockerCertPath","DOCKER_HOST=${dockerCertPath}"]) {
-			sh "export DTRIMAGE=${dockerRegistryRepoAppli} && cd ${codeEnv} && docker-compose config -e URL=${urlE2e} > docker-compose-deploy.yml"
-			sh "docker stack deploy --prune --compose-file=${codeEnv}/docker-compose-deploy.yml ${gitProjectName}"
-		}
-    }
+    //stage ('Deploiement UCP Docker ${codeEnv}') {
+	//	withEnv(['DOCKER_TLS_VERIFY=1',"DOCKER_CERT_PATH=$dockerCertPath","DOCKER_HOST=${dockerCertPath}"]) {
+	//		sh "export DTRIMAGE=${dockerRegistryRepoAppli} && cd ${codeEnv} && docker-compose config -e URL=${urlE2e} > docker-compose-deploy.yml"
+	//		sh "docker stack deploy --prune --compose-file=${codeEnv}/docker-compose-deploy.yml ${gitProjectName}"
+	//	}
+    //}
 }
