89dc547ff76b5730baed73cf31b1e1a794701794
Gilles Lantéri <glanteri@si2m.fr>
2018-10-17 15:10:42 +0200

    supp fichier prefix si2m ou sans ext

--- vars/si2mDicoCheckout.groovy
+++
@@ -1,9 +0,0 @@
-def call(def gitBitbucketUrl,def codeEnv, def gitProjectDico) {
-	stage('Checkout Dico ${codeEnv}'){
-      sh ("rm -f -r ${codeEnv}")
-      checkout([$class: 'GitSCM', branches: [[name: "*/${codeEnv}"]],
-      doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${codeEnv}"]],
-      submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'bitbucket-cloud', url: "${gitProjectDico}",refspec: "+refs/heads/${codeEnv}:refs/remotes/origin/${codeEnv}"]]])
-    }
-}
-	
