99ed0f47f0d44d188dd5f7c0258e7029a5caf26a
smurer <smurer@si2m.fr>
2018-10-17 15:03:32 +0000

    usilBuildMaven.groovy edited online with Bitbucket

--- vars/usilBuildMaven.groovy
+++ vars/usilBuildMaven.groovy
@@ -1,31 +1,33 @@
 def call(def dockerRegistryUrl,def dockerImageName,def nexusRepo,def gitBranchName,def trigrammeAppli){
-echo dockerRegistryUrl
 docker.withRegistry(dockerRegistryUrl) {
     docker.image(dockerImageName).inside {
-      stage('Build') {
-        sh 'npm ci --unsafe-perm'
-      }
-      stage('Qualimetry') {
-        parallel(
-          "Tests": {
-            sh 'npm run test:ci'
-          },
-          "Lint":{
-            sh 'npm run lint'
-          }
-        )
-      }
-      if (gitBranchName == 'master' || gitBranchName == 'develop') {
-        stage('NPM Pack') {
-          sh ('npm pack')
-        }
-
-        if(gitBranchName == 'master' && trigrammeAppli == 'BPP') {
-          stage('Publish Nexus') {
-            sh ('npm publish --registry ${nexusRepo}')
-          }
-        }
-      }
-    }
-  }
+		stage('MVN install') {
+            sh 'mvn install -Dconsole'
+			}
+		stage('Test Junit Maven') {
+			parallel(
+				"JUnit test": {
+					sh ('mvn test')
+					},
+				"Sonar":{
+					withSonarQubeEnv('SONARQUBE_USIL3') {
+					sh "mvn sonar:sonar -Dsonar.projectKey=${env.gitProjectName}"
+					}
+                }
+			)
+		}
+		stage('Quality Gate') {
+			def qg = waitForQualityGate()
+            if (qg.status != 'OK') {
+				error "Pipeline aborted due to quality gate failure: ${qg.status}"
+				}
+            }	
+		stage('Maven Pack') {
+            sh ('mvn clean package -DskipTests=true')
+            }
+        stage('Publish Nexus') {
+            sh ('mvn deploy')	
+			}
+		}
+	}
 }
