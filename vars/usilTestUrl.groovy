3d51d0cd1d90965a41051ea62f56cf86071ccc37
smurer <smurer@si2m.fr>
2018-10-23 11:15:40 +0000

    usilTestUrl.groovy edited online with Bitbucket

--- vars/usilTestUrl.groovy
+++ vars/usilTestUrl.groovy
@@ -1,12 +1,15 @@
 def call(def codeEnv) {       
-       stage('Check Availability') {            
+       stage('Check Supervision') {
+			def props = readProperties interpolate: false, file: "${codeEnv}/.env"
+			def urlEnv=props.URL;
+			def urlCheck="${urlEnv}/status"
               waitUntil {
                   try {         
-                      sh "curl -s --head  --request GET  localhost:8081/actuator/health | grep '200'"
+                      sh "curl -s --head  --request GET  ${urlCheck} | grep 'ok\|OK'"
                       return true
                   } catch (Exception e) {
-                        return false
+                      return false
                   }
            }
        }
-}
+	}
