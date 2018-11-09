b8e2d1dd567c6e7b2425198d73b1c6a07de74c42
smurer <smurer@si2m.fr>
2018-11-09 14:53:46 +0000

    usilBuildMaven.groovy edited online with Bitbucket

--- vars/usilBuildMaven.groovy
+++ vars/usilBuildMaven.groovy
@@ -1,17 +1,17 @@
 def call(def dockerRegistryUrl,def dockerImageName,def nexusRepo,def gitBranchName,def trigrammeAppli){
 docker.withRegistry(dockerRegistryUrl) {
-    docker.image(dockerImageName).inside('-v /appli/jenkins/settings.xml:/usr/share/maven/ref/settings.xml -v /appli/jenkins/mavenrepo:/var/maven/.m2' ) {
+    docker.image(dockerImageName).inside('-e MAVEN_CONFIG=/var/maven/.m2 -v /appli/jenkins/settings.xml:/usr/share/maven/ref/settings.xml -v /appli/jenkins/mavenrepo:/var/maven/.m2' ) {
 		stage('MVN install') {
             sh 'mvn install -Dconsole -Duser.home=/var/maven'
 			}
 		stage('Test Junit Maven') {
 			parallel(
 				"JUnit test": {
-					sh ('mvn test -Duser.home=/var/maven')
+					sh ('mvn test')
 					},
 				"Sonar":{
 					withSonarQubeEnv('SONARQUBE_USIL3') {
-					sh "mvn sonar:sonar -X -Dsonar.projectKey=${env.gitProjectName} -Duser.home=/var/maven"
+					sh "mvn sonar:sonar -Dsonar.projectKey=${env.gitProjectName}"
 					}
                 }
 			)
@@ -23,10 +23,10 @@ docker.withRegistry(dockerRegistryUrl) {
 				}
             }	
 		stage('Maven Pack') {
-            sh ('mvn clean package -DskipTests=true -Duser.home=/var/maven')
+            sh ('mvn clean package -DskipTests=true')
             }
         stage('Publish Nexus') {
-            sh ('mvn deploy -Duser.home=/var/maven')	
+            sh ('mvn deploy')	
 			}
 		}
 	}
