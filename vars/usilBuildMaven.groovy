e3188d9cfd95a99fad1c8d37a13e883f6c6eeb0b
smurer <smurer@si2m.fr>
2018-11-13 07:10:30 +0000

    usilBuildMaven.groovy edited online with Bitbucket

--- vars/usilBuildMaven.groovy
+++ vars/usilBuildMaven.groovy
@@ -12,7 +12,6 @@ docker.withRegistry(dockerRegistryUrl) {
 				"Sonar":{
 					withSonarQubeEnv('SONARQUBE_USIL3') {
 					sh "mvn sonar:sonar -Dsonar.projectKey=${env.gitProjectName} -Duser.home=/var/maven/.m2 -s /usr/share/maven/ref/settings.xml -X"
-					//SONAR_USER_HOME=/var/maven/.sonar mvn sonar:sonar -Dsonar.projectKey=${env.gitProjectName} -Duser.home=/var/maven"
 					}
                 }
 			)
