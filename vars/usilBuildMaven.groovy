b9ac4e49227da0bd6960e9dac2c8d438c39e6b47
smurer <smurer@si2m.fr>
2018-11-13 10:43:30 +0000

    usilBuildMaven.groovy edited online with Bitbucket

--- vars/usilBuildMaven.groovy
+++ vars/usilBuildMaven.groovy
@@ -26,7 +26,7 @@ docker.withRegistry(dockerRegistryUrl) {
             sh ('mvn clean package -DskipTests=true -Duser.home=/var/maven -s /usr/share/maven/ref/settings.xml')
             }
         stage('Publish Nexus') {
-            sh ('mvn deploy -Duser.home=/var/maven -s /usr/share/maven/ref/settings.xml -X')	
+            sh ('mvn deploy -Duser.home=/var/maven -s /usr/share/maven/ref/settings.xml')	
 			}
 		}
 	}
