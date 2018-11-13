6f7447dae47eb8e51db86968b1d8e69a86bc7bb3
smurer <smurer@si2m.fr>
2018-11-13 07:03:11 +0000

    usilBuildMaven.groovy edited online with Bitbucket

--- vars/usilBuildMaven.groovy
+++ vars/usilBuildMaven.groovy
@@ -2,16 +2,17 @@ def call(def dockerRegistryUrl,def dockerImageName,def nexusRepo,def gitBranchNa
 docker.withRegistry(dockerRegistryUrl) {
     docker.image(dockerImageName).inside('-e MAVEN_CONFIG=/var/maven/.m2 -v /appli/jenkins/settings.xml:/usr/share/maven/ref/settings.xml -v /appli/jenkins/mavenrepo:/var/maven/.m2:rw') {
 		stage('MVN install') {
-            sh 'mvn install -Dconsole -Duser.home=/var/maven'
+            sh 'mvn install -Dconsole -Duser.home=/var/maven/.m2'
 			}
 		stage('Test Junit Maven') {
 			parallel(
 				"JUnit test": {
-					sh ('mvn test -Duser.home=/var/maven')
+					sh ('mvn test -Duser.home=/var/maven/.m2')
 					},
 				"Sonar":{
 					withSonarQubeEnv('SONARQUBE_USIL3') {
-					sh "SONAR_USER_HOME=/var/maven/.sonar mvn sonar:sonar -Dsonar.projectKey=${env.gitProjectName} -Duser.home=/var/maven"
+					sh "mvn sonar:sonar -Dsonar.projectKey=$JOB_SONAR -Duser.home=/var/maven/.m2 -s /usr/share/maven/ref/settings.xml -X"
+					//SONAR_USER_HOME=/var/maven/.sonar mvn sonar:sonar -Dsonar.projectKey=${env.gitProjectName} -Duser.home=/var/maven"
 					}
                 }
 			)
