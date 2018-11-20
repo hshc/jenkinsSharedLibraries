c6b3515a60b75c1c6a2ee85139981b3d1c484e9f
smurer <smurer@si2m.fr>
2018-11-20 14:25:23 +0000

    usilProjectCheckout.groovy edited online with Bitbucket

--- vars/usilProjectCheckout.groovy
+++ vars/usilProjectCheckout.groovy
@@ -15,7 +15,6 @@ def call() {
 		env.codeAppli = sh(returnStdout: true, script: "echo '$env.gitProjectName' | awk -F'_' '{print \$2}'").trim()
 		env.projectName = sh(returnStdout: true, script: "echo '$env.gitProjectName' | awk -F'_' '{print \$3}'").trim()
 		env.gitTag = sh(returnStdout: true, script: "git tag -l | sort -V | tail -n 1 | cut -c 2-").trim()
-		echo env.BRANCH_NAME
 		env.gitBranchName=env.BRANCH_NAME
 	}
 }
