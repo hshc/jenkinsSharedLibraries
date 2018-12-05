7b6d56097df8037a4f5b76ef65ec673a6fd6feb4
Julien APPERT <jappert@si2m.fr>
2018-12-05 10:16:35 +0100

    chore(maven): add optionnal mvn test arguements and make sonar and junite tests sequential

--- vars/usilBuildMaven.groovy
+++ vars/usilBuildMaven.groovy
@@ -1,33 +1,28 @@
-def call(def dockerRegistryUrl,def dockerImageName,def nexusRepo,def gitBranchName,def trigrammeAppli){
+def call(def dockerRegistryUrl,def dockerImageName,def nexusRepo,def gitBranchName,def trigrammeAppli, def mvnTestOptionnalArgs){
 docker.withRegistry(dockerRegistryUrl) {
     docker.image(dockerImageName).inside('-e MAVEN_CONFIG=/var/maven/.m2 -v /appli/jenkins/settings.xml:/usr/share/maven/ref/settings.xml -v /appli/jenkins/mavenrepo:/var/maven/.m2:rw') {
 		stage('MVN compile') {
             sh 'mvn clean compile -DskipTests -Dconsole -Duser.home=/var/maven'
+		}
+		stage('Test Junit') {
+            sh "mvn test ${mvnTestOptionnalArgs} -Duser.home=/var/maven"
+		}
+		stage('Sonar') {
+			withSonarQubeEnv('SONARQUBE_USIL3') {
+				sh "mvn sonar:sonar -Dsonar.projectKey=${env.gitProjectName} -Duser.home=/var/maven/.m2 -s /usr/share/maven/ref/settings.xml"
 			}
-		stage('Test Junit Maven') {
-			parallel(
-				"JUnit test": {
-					sh ('mvn test -Duser.home=/var/maven')
-					},
-				"Sonar":{
-					withSonarQubeEnv('SONARQUBE_USIL3') {
-					sh "mvn sonar:sonar -Dsonar.projectKey=${env.gitProjectName} -Duser.home=/var/maven/.m2 -s /usr/share/maven/ref/settings.xml"
-					}
-                }
-			)
 		}
 		stage('Quality Gate') {
 			def qg = waitForQualityGate()
             if (qg.status != 'OK') {
 				error "Pipeline aborted due to quality gate failure: ${qg.status}"
-				}
-            }	
+			}
+		}	
 		stage('Maven Install') {
             sh ('mvn install -DskipTests -Duser.home=/var/maven -s /usr/share/maven/ref/settings.xml')
-            }
+		}
         stage('Publish Nexus') {
             sh ('mvn deploy -Duser.home=/var/maven -s /usr/share/maven/ref/settings.xml')	
-			}
 		}
 	}
 }
