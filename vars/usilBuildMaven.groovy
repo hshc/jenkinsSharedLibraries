4504c3d59b0ca54138b5c3719c4e5b3a46096ba4
Julien APPERT <jappert@si2m.fr>
2018-12-05 11:31:43 +0100

    chore(maven): update maven args name and use it in all maven cmd

--- vars/usilBuildMaven.groovy
+++ vars/usilBuildMaven.groovy
@@ -1,11 +1,11 @@
-def call(def dockerRegistryUrl,def dockerImageName,def nexusRepo,def gitBranchName,def trigrammeAppli, def mvnTestOptionnalArgs){
+def call(def dockerRegistryUrl,def dockerImageName,def nexusRepo,def gitBranchName,def trigrammeAppli, def mvnOptionnalArgs){
 docker.withRegistry(dockerRegistryUrl) {
     docker.image(dockerImageName).inside('-e MAVEN_CONFIG=/var/maven/.m2 -v /appli/jenkins/settings.xml:/usr/share/maven/ref/settings.xml -v /appli/jenkins/mavenrepo:/var/maven/.m2:rw') {
 		stage('MVN compile') {
-            sh 'mvn clean compile -DskipTests -Dconsole -Duser.home=/var/maven'
+			sh "mvn clean compile ${mvnOptionnalArgs} -DskipTests -Dconsole -Duser.home=/var/maven"
 		}
 		stage('Test Junit') {
-            sh "mvn test ${mvnTestOptionnalArgs} -Duser.home=/var/maven"
+			sh "mvn test ${mvnOptionnalArgs} -Duser.home=/var/maven"
 		}
 		stage('Sonar') {
 			withSonarQubeEnv('SONARQUBE_USIL3') {
@@ -14,15 +14,12 @@ docker.withRegistry(dockerRegistryUrl) {
 		}
 		stage('Quality Gate') {
 			def qg = waitForQualityGate()
-            if (qg.status != 'OK') {
+		if (qg.status != 'OK') {
 				error "Pipeline aborted due to quality gate failure: ${qg.status}"
 			}
 		}	
-		stage('Maven Install') {
-            sh ('mvn install -DskipTests -Duser.home=/var/maven -s /usr/share/maven/ref/settings.xml')
-		}
-        stage('Publish Nexus') {
-            sh ('mvn deploy -Duser.home=/var/maven -s /usr/share/maven/ref/settings.xml')	
+		stage('Publish Nexus') {
+			sh "mvn deploy ${mvnOptionnalArgs} -Duser.home=/var/maven -s /usr/share/maven/ref/settings.xml'""	
 		}
 	}
 }
