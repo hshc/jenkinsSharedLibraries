30fcaa67aa5d0b1f05c77771bb3d3e04c2ad5b82
Stephane MURER <smurer@si2m.fr>
2019-01-04 14:12:45 +0000

    usilSwarmDeploy.groovy édité en ligne via Bitbucket

--- vars/usilSwarmDeploy.groovy
+++ vars/usilSwarmDeploy.groovy
@@ -21,7 +21,7 @@ def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
 			sh "docker stack deploy --prune --compose-file=${codeEnv}/docker-compose-deploy.yml ${gitProjectName}_${codeEnv}"
 			sleep(time:30,unit:"SECONDS")
 			def checkService = sh(returnStdout: true, script: "docker stack services '${gitProjectName}'_'${codeEnv}' --format '{{.Replicas}}'").trim()
-			echo "Déploiement de la stack ${gitProjectName}_${codeEnv} la stack : ${checkService}"
+			echo "Deploiement de la stack ${gitProjectName}_${codeEnv}: ${checkService}"
 			if (checkService ==~ /^0\/.*$/)
 				{
 				echo '[FAILURE] Erreur de déploiement du service ou conteneur'
