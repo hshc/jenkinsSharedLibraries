2b799f4ac99e2f01cab7747af31a70da8081680b
Stephane MURER <smurer@si2m.fr>
2019-01-22 08:02:10 +0000

    passage de 30 à 60 sec

--- vars/usilSwarmDeploy.groovy
+++ vars/usilSwarmDeploy.groovy
@@ -19,7 +19,7 @@ def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
 	    	{
 			sh "export DTRIMAGE=${dockerRegistryRepoAppli} && cd ${codeEnv} && docker-compose config > docker-compose-deploy.yml"
 			sh "docker stack deploy --prune --compose-file=${codeEnv}/docker-compose-deploy.yml ${gitProjectName}_${codeEnv}"
-			sleep(time:30,unit:"SECONDS")
+			sleep(time:60,unit:"SECONDS")
 			def checkService = sh(returnStdout: true, script: "docker stack services '${gitProjectName}'_'${codeEnv}' --format '{{.Replicas}}'").trim()
 			echo "Deploiement de la stack ${gitProjectName}_${codeEnv}: ${checkService}"
 			if (checkService ==~ /^0\/.*$/)
