7d99cd176ccdcc2c29ee825abbfb66d1f80154f3
Stephane MURER <smurer@si2m.fr>
2019-01-23 16:58:43 +0000

    usilBuildNodejs.groovy édité en ligne via Bitbucket

--- vars/usilBuildNodejs.groovy
+++ vars/usilBuildNodejs.groovy
@@ -2,7 +2,7 @@ def call(def dockerRegistryUrl,def dockerImageName,def nexusRepo,def gitBranchNa
 docker.withRegistry(dockerRegistryUrl) {
     docker.image(dockerImageName).inside("--entrypoint=''") {
       stage('Build') {
-        sh "npm ci --registry ${nexusRepoNpm}"
+        sh "npm ci --registry '${nexusRepoNpm}'"
         if (isBuildRun == true) {
           sh 'npm run build:prod'
         }
@@ -24,7 +24,7 @@ docker.withRegistry(dockerRegistryUrl) {
 
         if(gitBranchName == 'master' && trigrammeAppli == 'BPP') {
           stage('Publish Nexus') {
-            sh ("npm publish --registry ${nexusRepoNpmPublish}")
+            sh ("npm publish --registry '${nexusRepoNpmPublish}'")
           }
         }
       }
