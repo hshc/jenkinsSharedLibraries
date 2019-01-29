aed67261e0da64ca9ea385c0dcef53df93ecbfb8
Stephane MURER <smurer@si2m.fr>
2019-01-29 14:46:09 +0000

    usilDtrSearchRepo.groovy créé en ligne via Bitbucket

---
+++ vars/usilDtrSearchRepo.groovy
@@ -0,0 +1,20 @@
+def dockerSearchRepo(def trigramme) { 	
+		script {
+			def response = httpRequest authentication: 'DockerDTR', url: "https://dtr.docker.si2m.tec/api/v0/index/dockersearch?q=${trigramme}", ignoreSslErrors:true
+			def json = new JsonSlurper().parseText(response.content)
+			def listRepo = []
+			for (result in json.results) {
+			    if (result.name =~ /^repo-store.*$/) {
+                    listRepo.add(result.name)
+			        }
+                }
+			if listRepo.isEmpty()
+				{
+				return 'noRepo'
+				}
+			else
+				{
+				return listRepo
+				}
+			}
+		}
