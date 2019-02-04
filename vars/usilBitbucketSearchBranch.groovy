ccd0cd7752fac291eab78c0b3b580aca311575cd
Stephane MURER <smurer@si2m.fr>
2019-02-04 07:18:05 +0000

    usilBitbucketSearchBranch.groovy édité en ligne via Bitbucket

--- vars/usilBitbucketSearchBranch.groovy
+++ vars/usilBitbucketSearchBranch.groovy
@@ -2,6 +2,9 @@ import groovy.json.JsonSlurper
 def call(def repo) { 	
 		script {
 			def response = httpRequest authentication: 'jenkins-bitbucket-common-creds', url: "https://api.bitbucket.org/2.0/repositories/si2m/${repo}_dico/refs/branches", ignoreSslErrors:true
+			echo '***************'
+			echo response.status)
+			echo '***************'
 			def json = new JsonSlurper().parseText(response.content)
 			def ListEnv = []
 			for (result in json.values) {
