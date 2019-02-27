2d16839e3440f36fd3a27f6c7e913751c1a90948
Stephane MURER <smurer@si2m.fr>
2019-02-27 07:06:45 +0000

    usilDtrSearchRepo.groovy édité en ligne via Bitbucket

--- vars/usilDtrSearchRepo.groovy
+++ vars/usilDtrSearchRepo.groovy
@@ -1,7 +1,8 @@
 import groovy.json.JsonSlurper
 def call(def trigramme) { 	
 		script {
-			def response = httpRequest authentication: 'DockerDTR', url: "https://dtr.docker.si2m.tec/api/v0/index/dockersearch?q=${trigramme}", ignoreSslErrors:true
+		    def request= trigramme.toLowerCase()
+			def response = httpRequest authentication: 'DockerDTR', url: "https://dtr.docker.si2m.tec/api/v0/index/dockersearch?q=${request}", ignoreSslErrors:true
 			def json = new JsonSlurper().parseText(response.content)
 			def listRepo = []
 			for (result in json.results) {
