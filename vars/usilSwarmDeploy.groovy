6d88ff9fb330b6289ab7c59d8b1b964880a8098a
si2m_gqv <smurer@si2m.fr>
2019-06-26 15:44:11 +0200

    no message

--- vars/usilSwarmDeploy.groovy
+++ vars/usilSwarmDeploy.groovy
@@ -32,7 +32,7 @@ def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
 			 println mydata.services.getClass()
 			 println mydata.services
 			 println mydata.services.keySet()
-			String nomService : mydata.services.keySet()
+			 def nomService = mydata.services.keySet()
 
 			 println "key" + nomService
 			
