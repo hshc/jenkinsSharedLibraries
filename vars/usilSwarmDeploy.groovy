0cd18fc26818ae0b828f418ce6e5a11b9f0e9318
si2m_gqv <smurer@si2m.fr>
2019-06-26 15:22:59 +0200

    no message

--- vars/usilSwarmDeploy.groovy
+++ vars/usilSwarmDeploy.groovy
@@ -35,7 +35,8 @@ def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
 			 def nomService = mydata.services.keySet()
 			 println "key" + nomService
 			
-            println mydata.services.mydata.services.get(nomService).deploy.labels
+            println mydata.services.mydata.services.get(nomService)
+			//.deploy.labels
 
 			// for (String key : mydata.services.keySet()) {
 			//	 println(key +"   clé  " + mydata.services.get(key))
