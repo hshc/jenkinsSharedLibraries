d14c70a251730bf656dd17e8d9016c8f15b8471b
si2m_gqv <smurer@si2m.fr>
2019-06-26 13:37:33 +0200

    no message

--- vars/usilSwarmDeploy.groovy
+++ vars/usilSwarmDeploy.groovy
@@ -31,8 +31,9 @@ def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
 			  def nomService = mydata.services =~/(?<=\{)(.*?)(?=\{image)/
 			  //assert nomService instanceof java.util.regex.Matcher
 			  println "nom du Service"
-			  println nomService.matcher[0]
-			  
+			  println nomService[0]
+			  println nomService[0][1]
+			 
 			 // println mydata.services.appli.image
 			 // println mydata.services.appli.deploy.labels
 			 def arrayLabels=mydata.services.appli.deploy.labels as String[]
