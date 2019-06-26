a4881809d76eb94781674c4e8c9b587915c494a9
si2m_gqv <smurer@si2m.fr>
2019-06-26 12:35:38 +0200

    no message

--- vars/usilSwarmDeploy.groovy
+++ vars/usilSwarmDeploy.groovy
@@ -24,7 +24,8 @@ def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
 			  println mydata.version
 			  println mydata.services
 			  // println (mydata.services =~ /(?<=\{)(.*?)(?={image)/ )
-			  println (mydata.services =~ /(?<={)(.*?)(?={image)/)
+			  println (mydata.services =~ /(?<=\{)(.*?)(?=\{image)/)
+			  //(?<={)(.*)(?=\={image)
 			  
 			  //def nomService = (mydata.services =~ /(?<=\{)(.*?)(?={image)/ )
 			  //println "nom du Service"
