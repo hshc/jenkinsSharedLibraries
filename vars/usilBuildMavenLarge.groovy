5e4d8b5fc34cad36ac644c0b4e061effff827460
idei010 <glanteri@si2m.fr>
2019-07-25 16:35:08 +0200

    ajout temp lib usilBuildMavenLarge

---
+++ vars/usilBuildMavenLarge.groovy
@@ -0,0 +1,29 @@
+def call(def dockerRegistryUrl,def dockerImageName,def nexusRepo,def gitBranchName,def trigrammeAppli, def mvnOptionnalArgs=''){
+	docker.withRegistry(dockerRegistryUrl) {
+		docker.image(dockerImageName).inside('-e MAVEN_CONFIG=/var/maven/.m2 -v /appli/jenkins/settings.xml:/usr/share/maven/ref/settings.xml -v /appli/jenkins/mavenrepo:/var/maven/.m2:rw') {
+			stage('MVN compile') {
+				sh "mvn clean compile ${mvnOptionnalArgs} -DskipTests -Dconsole -Duser.home=/var/maven"
+			}
+			stage('Test Junit') {
+				sh "mvn test ${mvnOptionnalArgs} -Duser.home=/var/maven"
+			}
+			stage('Sonar (large)') {
+				withSonarQubeEnv('SONARQUBE_USIL3') {
+					sh 'echo $MAVEN_OPTS'
+                    sh 'export MAVEN_OPTS="-Xmx3000m"'
+					sh 'echo $MAVEN_OPTS'
+                    sh "mvn sonar:sonar -Dsonar.projectKey=${env.gitProjectName} -Duser.home=/var/maven/.m2 -s /usr/share/maven/ref/settings.xml"
+				}
+			}
+			stage('Quality Gate') {
+				def qg = waitForQualityGate()
+				if (qg.status != 'OK') {
+					error "Pipeline aborted due to quality gate failure: ${qg.status}"
+				}
+			}
+			stage('Publish Nexus') {
+				sh "mvn deploy ${mvnOptionnalArgs} -Duser.home=/var/maven -s /usr/share/maven/ref/settings.xml"
+			}
+		}
+	}
+}
