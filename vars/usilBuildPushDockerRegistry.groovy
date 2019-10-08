33fe858cb8066077a9ef7c455399a6c2c6cab2b8
Stephane MURER <smurer@si2m.fr>
2019-10-08 09:33:35 +0000

    usilBuildPushDockerRegistry.groovy édité en ligne via Bitbucket

--- vars/usilBuildPushDockerRegistry.groovy
+++ vars/usilBuildPushDockerRegistry.groovy
@@ -7,7 +7,7 @@ def call(def dockerRegistryUrl,def dockerRegistryUser, def dockerRegistryRepoApp
 			imageDocker.push()
         }
       }
-    def urlRepo= "${dockerRegistryUrl}/${dockerRegistryRepoAppli.replaceAll(/:.*$/, "")}"
+    def urlRepo= "${dockerRegistryUrl}/api/v0/repositories/${dockerRegistryRepoAppli.replaceAll(/:.*$/, "")}"
     println urlRepo
     def response = httpRequest authentication: 'DockerDTR', acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'PATCH', requestBody: '{"visibility": "public"}', url: urlRepo, ignoreSslErrors:true
     println response
