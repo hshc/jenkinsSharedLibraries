0d80e1ba5634e2e9988e35bf92dc1a21f442ef71
Stephane MURER <smurer@si2m.fr>
2019-10-10 14:59:15 +0000

    usilBuildPushDockerRegistry.groovy édité en ligne via Bitbucket

--- vars/usilBuildPushDockerRegistry.groovy
+++ vars/usilBuildPushDockerRegistry.groovy
@@ -10,7 +10,6 @@ def call(def dockerRegistryUrl,def dockerRegistryUser, def dockerRegistryRepoApp
     // connexion à la dtr via API pour changer la visibilité du repo : visibility public 
     def urlRepo= "${dockerRegistryUrl}/api/v0/repositories/${dockerRegistryRepoAppli.replaceAll(/:.*$/, "")}"
     def response = httpRequest authentication: 'DockerDTR', acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'PATCH', requestBody: '{"visibility": "public"}', url: urlRepo, ignoreSslErrors:true
-    println response.status
     if (response.status != 200) {
       println "Repo sur la DTR inexistant"
       currentBuild.result = 'FAILURE'
