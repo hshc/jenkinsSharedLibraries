ed8680a0c6184ccd237752fd1ff919fe729d2427
Younes DJOUAMBI <ydjouambi@si2m.fr>
2019-12-12 22:01:05 +0100

    add Ci SBT

---
+++ vars/usilBuildSbt.groovy
@@ -0,0 +1,23 @@
+def call(def dockerRegistryUrl,def dockerImageName,def nexusRepo,def gitBranchName,def trigrammeAppli, def sbtOptionnalArgs=''){
+	docker.withRegistry(dockerRegistryUrl) {
+		docker.image(dockerImageName).inside('-v /var/lib/jenkins/.sbt:/root/.sbt -v /var/lib/jenkins/.ivy2:/root/.ivy2 ') {
+			stage('SBT Compile Coverage Test') {
+			     sh "${sbtOptionnalArgs} sbt -Dsbt.global.base=.sbt -Dsbt.ivy.home=/home/jenkins/.ivy2 -Divy.home=/home/jenkins/.ivy2 compile coverage jacoco:cover coverageReport coverageOff"
+			}
+			stage('Sonar') {
+				withSonarQubeEnv('SONARQUBE_USIL3') {
+				  sh "sbt sonarScan -Dsonar.projectKey=${env.gitProjectName} -Dsbt.global.base=.sbt -Dsbt.ivy.home=/home/jenkins/.ivy2 -Divy.home=/home/jenkins/.ivy2"
+				}
+			}
+			stage('Quality Gate') {
+                def qg = waitForQualityGate()
+                if (qg.status != 'OK') {
+                    error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                }
+            }
+			stage('SBT Package') {
+				sh "${sbtOptionnalArgs} sbt -Dsbt.global.base=.sbt -Dsbt.ivy.home=/home/jenkins/.ivy2 -Divy.home=/home/jenkins/.ivy2 universal:packageZipTarball"
+			}
+		}
+	}
+}
