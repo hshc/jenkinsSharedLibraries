12f07fe086890832f4d40f57263881d2d4216fe1
si2m_gqv <smurer@si2m.fr>
2020-03-05 10:04:02 +0100

    no message

--- vars/usilVaultCheckout.groovy
+++ vars/usilVaultCheckout.groovy
@@ -1,10 +1,12 @@
+// la fonction reçoie en entrée l'URL Vault, l'ID Vault, le code environnement, le trigramme applicatif, 
+// nom du repo git, le tag git, la clé vaultKey par défaut prend la valeur value.yaml
 def call(def vaultUrl, def vaultId,def codeEnv,def trigrammeAppli, def gitProjectName, def gitTag, def vaultKey='value.yaml' ) {
 stage("Récupération env Vault env:${codeEnv} version:${gitTag}"){
+      // Utilisation du plugin Vault pour aller récupérer la valeur dans kv/TRIGRAMME/trigramme_codeappli_description/tag la clé par défaut est value.yaml
       withVault(configuration: [timeout: 60, vaultCredentialId: vaultId, vaultUrl: vaultUrl], vaultSecrets: [[path: "kv/${trigrammeAppli.toUpperCase()}/${gitProjectName.toLowerCase()}/${gitTag}", secretValues: [[envVar: 'vaultValue', vaultKey: "${vaultKey}"]]]]) {
-       sh "rm -f ${vaultKey}"
-       writeYaml file: "${env.WORKSPACE}/${vaultKey}", data: vaultValue
-       sh "rm -f test.yaml"
-       writeFile file: "${env.WORKSPACE}/test.yaml", text: vaultValue
+        sh "rm -f ${vaultKey}"
+        // ecriture du fichier à partie de la clé lue sur Vault
+        writeFile file: "${env.WORKSPACE}/${vaultKey}", text: vaultValue
     }
   }
 }
