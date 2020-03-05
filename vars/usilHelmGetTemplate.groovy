36d61daffd6f95f7dc35d7bcaf669ce214c70c3b
si2m_gqv <smurer@si2m.fr>
2020-03-05 15:28:14 +0100

    no message

--- vars/usilHelmGetTemplate.groovy
+++ vars/usilHelmGetTemplate.groovy
@@ -1,8 +1,11 @@
 def call(def chartTemplateName,def gitBitbucketUrl) {
 	stage("Checkout Template Helm"){
       sh ("rm -f -r ${chartTemplateName}")
-      checkout([$class: 'GitSCM', branches: [[name: "*/${chartTemplateName}"]],
-      doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${chartTemplateName}"]],
-      submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'jenkins-bitbucket-common-creds', url: "${gitBitbucketUrl}${chartTemplateName}"]]])
+      checkout([$class: 'GitSCM', branches: [[name: '*/master']], 
+      doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: 
+      [[credentialsId: 'jenkins-bitbucket-common-creds', url: 'https://bitbucket.org/si2m/chart_nodejs']]])
+      // checkout([$class: 'GitSCM', branches: [[name: "*/${chartTemplateName}"]],
+      // doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${chartTemplateName}"]],
+      // submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'jenkins-bitbucket-common-creds', url: "${gitBitbucketUrl}${chartTemplateName}"]]])
     }
 }
