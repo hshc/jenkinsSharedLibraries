e4ab91c8d5d1b9ad73d6c5f7e3a1f667396166ca
si2m_gqv <smurer@si2m.fr>
2020-03-05 13:54:20 +0100

    no message

--- vars/usilVaultCheckout.groovy
+++ vars/usilVaultCheckout.groovy
@@ -4,6 +4,7 @@ def call(def vaultUrl, def vaultId,def codeEnv,def trigrammeAppli, def gitProjec
 stage("Récupération env Vault env:${codeEnv} version:${gitTag}"){
       // Utilisation du plugin Vault pour aller récupérer la valeur dans kv/TRIGRAMME/trigramme_codeappli_description/tag la clé par défaut est value.yaml
       withVault(configuration: [timeout: 60, vaultCredentialId: vaultId, vaultUrl: vaultUrl], vaultSecrets: [[path: "kv/${trigrammeAppli.toUpperCase()}/${gitProjectName.toLowerCase()}/${gitTag}", secretValues: [[envVar: 'vaultValue', vaultKey: "${vaultKey}"]]]]) {
+        // véirification si le path contient un / à la fin si oui on ne fait sinon on l'ajoute
         vaultKeyPath += vaultKeyPath?.endsWith('/') ? '' : '/'
         sh "rm -f ${vaultKeyPath}${vaultKey}"
         // écriture du fichier à partie de la clé lue sur Vault
