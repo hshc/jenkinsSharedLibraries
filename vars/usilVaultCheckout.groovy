074cefa902226cbb6ff22f13efabece298bfcf81
si2m_gqv <smurer@si2m.fr>
2020-03-05 09:46:30 +0100

    no message

--- vars/usilVaultCheckout.groovy
+++ vars/usilVaultCheckout.groovy
@@ -1,7 +1,7 @@
 def call(def vaultUrl, def vaultId,def codeEnv,def trigrammeAppli, def gitProjectName, def gitTag, def vaultKey='value.yaml' ) {
 stage("Récupération env Vault env:${codeEnv} version:${gitTag}"){
       withVault(configuration: [timeout: 60, vaultCredentialId: vaultId, vaultUrl: vaultUrl], vaultSecrets: [[path: "kv/${trigrammeAppli.toUpperCase()}/${gitProjectName.toLowerCase()}/${gitTag}", secretValues: [[envVar: 'vaultValue', vaultKey: "${vaultKey}"]]]]) {
-       sh "rm -f ${vaultValue}"
+       sh "rm -f ${vaultKey}"
        writeYaml file: "${env.WORKSPACE}/${vaultKey}", data: vaultValue
        sh "rm -f test.yaml"
        writeFile file: "${env.WORKSPACE}/test.yaml", text: vaultValue
