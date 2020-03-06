9ea8a2dc44b2392f6934ec50b25eae3446892bf0
si2m_gqv <smurer@si2m.fr>
2020-03-06 11:18:26 +0100

    no message

--- vars/usilHelmCreateChart.groovy
+++ vars/usilHelmCreateChart.groovy
@@ -1,17 +1,8 @@
-import groovy.io.FileType
 def call(def chartTemplateName,def gitProjectName) {
-println GroovySystem.version
-
 stage("Create Chart Helm"){
-  new File(env.WORKSPACE).eachFileRecurse(FileType.FILES) {
-  println it
-  //  file ->
-   //   list << file
-  //  println file
-    //if(it.name.endsWith('*.yaml')) {
-     //   println it
-     //   }
-    }
+  def findTemplate = sh ( script : 'find ./chart_nodejs -maxdepth 1 -type f -name *.yaml',returnStdout: true).trim()       
+  println findTemplate
+
   }
 }
   
