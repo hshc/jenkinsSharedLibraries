539e82ec542824c96c13f34dadf7934e7be1011c
si2m_gqv <smurer@si2m.fr>
2020-03-06 11:36:23 +0100

    no message

--- vars/usilHelmCreateChart.groovy
+++ vars/usilHelmCreateChart.groovy
@@ -3,7 +3,12 @@ stage("Create Chart Helm"){
   def findTemplate = sh ( script : 'find ./chart_nodejs -maxdepth 1 -type f -name *.yaml',returnStdout: true).trim()       
   println findTemplate
   println findTemplate.getClass()
-
+  }
+  findTemplate.eachLine {
+    line, count ->
+    //if (count == 0) {
+        println "line $count: $line"  // Output: line 0: Groovy is closely related to Java,
+   // }
   }
 }
   
