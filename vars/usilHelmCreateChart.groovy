bf9df2ba6e68a7b9ff0b15ceade58db73ca5fdae
si2m_gqv <smurer@si2m.fr>
2020-03-06 10:02:33 +0100

    no message

--- vars/usilHelmCreateChart.groovy
+++ vars/usilHelmCreateChart.groovy
@@ -1,5 +1,7 @@
-import groovy.io.FileType.FILES
+import groovy.io.FileType
 def call(def chartTemplateName,def gitProjectName) {
+println GroovySystem.version
+
 stage("Create Chart Helm"){
   def dir = new File("${env.WORKSPACE}/${chartTemplateName}")
   dir.eachFileRecurse(FILES) {
