f3bf7a347e24b4881d37655936d4b3b0d77d2188
si2m_gqv <smurer@si2m.fr>
2020-03-06 16:04:28 +0100

    no message

--- vars/usilHelmGetTemplate.groovy
+++ vars/usilHelmGetTemplate.groovy
@@ -1,6 +1,8 @@
 def call(def chartTemplateName,def gitBitbucketUrl) {
-	stage("Checkout Template Helm"){
+	stage("Checkout Template Helm : ${chartTemplateName}"){
+      // suppression du répertoire 
       sh ("rm -f -r ${chartTemplateName}")
+      // récupération du modèle chart Helm sur le repo GIT et stockage dans le dossier avec le nom du modèle 
       checkout([$class: 'GitSCM', branches: [[name: '*/master']], 
       doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${chartTemplateName}"]], submoduleCfg: [], userRemoteConfigs: 
       [[credentialsId: 'jenkins-bitbucket-common-creds', url: 'https://bitbucket.org/si2m/chart_nodejs']]])
--- vars/usilHelmdeploy.groovy
+++ vars/usilHelmdeploy.groovy
@@ -1,3 +1,13 @@
 def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
+ // A coder
+ // récupération de la conf dédié à l'environnement Kub
+
+ // Lancement de l'image docker Helm / kub 
+
+ // la ligne de commande pour installer 
+ // il peut être inétressant de stocker le fichier template
+ // "helm template ${gitProjectName} --set modelTemplate.image.repository="${dockerRegistryRepoAppli}" --set modelTemplate.environment="${codeEnv}" --set modelTemplate.name=${gitProjectName}"
+
+ // "helm install ${gitProjectName} --namespace ${gitProjectName} --set modelTemplate.image.repository="${dockerRegistryRepoAppli}" --set modelTemplate.environment="${codeEnv}" --set modelTemplate.name=${gitProjectName}"
 
 }
