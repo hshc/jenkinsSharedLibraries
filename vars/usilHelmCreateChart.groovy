61ed31df0a4485106e9071329b11848138ee9dc2
si2m_gqv <smurer@si2m.fr>
2020-03-06 09:25:05 +0100

    no message

--- vars/usilHelmCreateChart.groovy
+++ vars/usilHelmCreateChart.groovy
@@ -1,15 +1,13 @@
 import groovy.io.FileType
 def call(def chartTemplateName,def gitProjectName) {
-	stage("Create Chart Helm"){
-    // liste les fichiers yaml template
-    def list = []
-    def dir = new File("${env.WORKSPACE}/${chartTemplateName}")
-    println dir
-    dir.eachFileRecurse (FileType.FILES) { 
-      file -> list << file
-      } 
-    list.each {
-    println it.path
+stage("Create Chart Helm"){
+  import static groovy.io.FileType.FILES
+  def dir = new File("${env.WORKSPACE}/${chartTemplateName}")
+  dir.eachFileRecurse(FILES) {
+    if(it.name.endsWith('.yaml')) {
+        println it
+      }
     }
   }
 }
+
