0c506555589186854ae9fe42d50328556eba6947
si2m_gqv <smurer@si2m.fr>
2020-03-06 10:09:40 +0100

    no message

--- vars/usilHelmCreateChart.groovy
+++ vars/usilHelmCreateChart.groovy
@@ -3,7 +3,7 @@ def call(def chartTemplateName,def gitProjectName) {
 println GroovySystem.version
 
 stage("Create Chart Helm"){
-  new File("${env.WORKSPACE}/${chartTemplateName}").traverse(type: groovy.io.FileType.FILES, nameFilter: ~/patternRegex/) { it ->
+  new File("${env.WORKSPACE}/${chartTemplateName}").traverse(type: groovy.io.FileType.FILES, nameFilter: "*.yaml") { it ->
     println it
     }
   }
