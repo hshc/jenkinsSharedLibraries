450d28cf8761a98add8b2c1e2369f88eebb736dc
si2m_gqv <smurer@si2m.fr>
2020-03-06 10:26:46 +0100

    no message

--- vars/usilHelmCreateChart.groovy
+++ vars/usilHelmCreateChart.groovy
@@ -3,7 +3,7 @@ def call(def chartTemplateName,def gitProjectName) {
 println GroovySystem.version
 
 stage("Create Chart Helm"){
-  new File('.').eachFileRecurse(FileType.FILES) {
+  new File("${env.WORKSPACE}/${chartTemplateName}/").eachFileRecurse(FileType.FILES) {
     if(it.name.endsWith('.yaml')) {
         println it
         }
