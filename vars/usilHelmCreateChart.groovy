11eadef6d50f0fc602a4d587e1a36792d36f5b05
si2m_gqv <smurer@si2m.fr>
2020-03-06 11:29:03 +0100

    no message

--- vars/usilHelmCreateChart.groovy
+++ vars/usilHelmCreateChart.groovy
@@ -2,6 +2,7 @@ def call(def chartTemplateName,def gitProjectName) {
 stage("Create Chart Helm"){
   def findTemplate = sh ( script : 'find ./chart_nodejs -maxdepth 1 -type f -name *.yaml',returnStdout: true).trim()       
   println findTemplate
+  println findTemplate.getClass()
 
   }
 }
