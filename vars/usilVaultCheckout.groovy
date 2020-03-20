9509405e0f8912080a23957b982170c3eed6cb6c
si2m_gqv <smurer@si2m.fr>
2020-03-20 17:03:14 +0100

    no message

--- vars/usilVaultCheckout.groovy
+++ vars/usilVaultCheckout.groovy
@@ -1,9 +1,9 @@
 // la fonction reçoie en entrée l'URL Vault, l'ID Vault, le code environnement, le trigramme applicatif, 
 // nom du repo git, le tag git, le path ou est stocké le fichier récupéré sur Vault, la clé vaultKey par défaut prend la valeur value.yaml, 
-def call(def vaultUrl, def vaultId,def codeEnv,def trigrammeAppli, def gitProjectName, def gitTag, def vaultKeyPath, def vaultKey='value.yaml' ) {
+def call(def vaultUrl, def vaultId,def codeEnv,def trigrammeAppli, def gitProjectName, def gitTag, def vaultKeyPath, def vaultKey='values.yaml' ) {
 stage("Récupération env Vault env:${codeEnv} version:${gitTag}"){
       // Utilisation du plugin Vault pour aller récupérer la valeur dans kv/TRIGRAMME/trigramme_codeappli_description/tag la clé par défaut est value.yaml
-      withVault(configuration: [timeout: 60, vaultCredentialId: vaultId, vaultUrl: vaultUrl], vaultSecrets: [[path: "kv/${trigrammeAppli.toUpperCase()}/${gitProjectName.toLowerCase()}/${gitTag}", secretValues: [[envVar: 'vaultValue', vaultKey: "${vaultKey}"]]]]) {
+      withVault(configuration: [timeout: 60, vaultCredentialId: vaultId, vaultUrl: vaultUrl], vaultSecrets: [[path: "kv/${trigrammeAppli.toUpperCase()}/${gitProjectName.toLowerCase()}/${codeEnv}/${gitTag}", secretValues: [[envVar: 'vaultValue', vaultKey: "${vaultKey}"]]]]) {
         // véirification si le path contient un / à la fin si oui on ne fait sinon on l'ajoute
         vaultKeyPath += vaultKeyPath?.endsWith('/') ? '' : '/'
         sh "rm -f ${vaultKeyPath}${vaultKey}"
