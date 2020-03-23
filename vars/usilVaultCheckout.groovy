8d22ef2426eabef503c1d78526d4ac9c950ae4a7
si2m_gqv <smurer@si2m.fr>
2020-03-23 11:02:01 +0100

    no message

--- vars/usilVaultCheckout.groovy
+++ vars/usilVaultCheckout.groovy
@@ -11,6 +11,11 @@ stage("Récupération env Vault env:${codeEnv} version:${gitTag}"){
         sh "rm -f ${vaultKeyPath}${vaultKey}"
         // écriture du fichier à partie de la clé lue sur Vault
         writeFile file: "${env.WORKSPACE}/${vaultKeyPath}${vaultKey}", text: vaultValue
+        def mydata = readYaml file: "${env.WORKSPACE}/${vaultKeyPath}${vaultKey}"
+        def nomServiceKO = mydata."${gitProjectName}".keySet()
+        def nomServiceOK = mydata."${gitProjectName.replaceAll('_','-')}".keySet()
+        println "nomServiceKO : ${nomServiceKO}"
+        println "nomServiceOK : ${nomServiceOK}"
     }
   }
 }
