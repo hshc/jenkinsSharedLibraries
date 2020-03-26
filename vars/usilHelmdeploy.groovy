68cd2a0ee9fb2afc9ed7f2147219feceaf52563c
imqo007 <fpaillard@si2m.fr>
2020-03-26 12:44:39 +0100

    Implémentation helm deploy

--- vars/usilHelmdeploy.groovy
+++ vars/usilHelmdeploy.groovy
@@ -1,12 +1,11 @@
 def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
- // A coder
- // récupération de la conf dédié à l'environnement Kub
-
- // Lancement de l'image docker Helm / kub 
+// récupération de la conf dédié à l'environnement Kub
+trigramme = ${gitProjectName}.substring(0,2)
+// Lancement de l'image docker Helm / kub 
 
  
- // il peut être inétressant de stocker le fichier template
- helmTemplate = "helm template ${gitProjectName} " + 
+// il peut être inétressant de stocker le fichier template
+helmTemplate = "helm template ${gitProjectName} " + 
        "--set modelTemplate.image.repository=${dockerRegistryRepoAppli} " + 
        "--set modelTemplate.environment=${codeEnv} " +
        "--set modelTemplate.name=${gitProjectName} " +
@@ -14,9 +13,16 @@ def call(def codeEnv,def dockerRegistryRepoAppli,def gitProjectName) {
        "--set secretName=${codeEnv}-mycloud-secret " + 
        "--set modelTemplate.version=latest " + 
        "> ${gitProjectName}.yaml"
- kubeApply = "kubectl apply --namespace ${gitProjectName}.substring(0,2) -f ${gitProjectName}.yaml"
- sh "$helmTemplate"
- sh "$kubeApply"
+kubeApply = "kubectl apply --namespace ${trigramme} -f ${gitProjectName}.yaml"
+sh "k config set-context cluster--n ${trigramme}"
+sh "$helmTemplate"
+sh "$kubeApply"
+
+podLog = sh (script : "kubectl logs -l app=${nomContainer} --tail 1", returnStdout: true)
+deploymentStatus = sh "kubectl rollout status ${gitProjectName}"
+
+ 
+echo "log ${nomContainer} ${dockerLog}"
  
  // la ligne de commande pour installer 
  // "helm install ${gitProjectName} --namespace ${gitProjectName} --set modelTemplate.image.repository="${dockerRegistryRepoAppli}" --set modelTemplate.environment="${codeEnv}" --set modelTemplate.name=${gitProjectName}"
