907ed6263c03e0d10c1305c6a2dcab4d268e0f43
si2m_gqv <smurer@si2m.fr>
2020-04-07 11:29:00 +0200

    Modif pour prise en compte règle de nommage sans _

--- vars/usilHelmCreateChart.groovy
+++ vars/usilHelmCreateChart.groovy
@@ -1,5 +1,5 @@
-def call(def chartTemplateName,def gitProjectName) {
-stage("Create Chart Helm : ${gitProjectName} template: ${chartTemplateName}"){
+def call(def chartTemplateName,def helmProjectName) {
+stage("Create Chart Helm : ${helmProjectName} template: ${chartTemplateName}"){
   // on liste les fichiers présent dans le repertoire ${chartTemplateName}/templates
   def findTemplate = sh ( script : "find ${chartTemplateName}/templates -maxdepth 1 -type f -name *.yaml",returnStdout: true).trim()       
   // on créé une liste contenant la liste des fichiers avec leurs paths
@@ -10,13 +10,13 @@ stage("Create Chart Helm : ${gitProjectName} template: ${chartTemplateName}"){
   listeFichierTemplate.each {
     // lecture de du fichier récupéré sur la closure it dont le contenu est stocké dans contenuTemplateYaml
     contenuTemplateYaml = readFile it
-    // on personnalise le contenu des variables du template modelTemplate par le nom du service (gitProjectName)
-    contenuTemplateYaml = contenuTemplateYaml.replaceAll( 'modelTemplate', gitProjectName )
+    // on personnalise le contenu des variables du template modelTemplate par le nom du service (helmProjectName)
+    contenuTemplateYaml = contenuTemplateYaml.replaceAll( 'modelTemplate', helmProjectName )
     // on supprime le path 
     def templateCible=listeFichierTemplate[index].replaceAll("${chartTemplateName}/templates/",'')
-    println "Préparation pour le service ${gitProjectName} du composant Helm : ${gitProjectName}_${templateCible}"
-    // on écrit le fichier dans le repertoire gitProjectName/templates/ et on préfixe les fichiers avec gitProjectName
-    writeFile file: "${gitProjectName}/templates/${gitProjectName}-${templateCible}", text: contenuTemplateYaml
+    println "Préparation pour le service ${appliHelmName} du composant Helm : ${helmProjectName}_${templateCible}"
+    // on écrit le fichier dans le repertoire helmProjectName/templates/ et on préfixe les fichiers avec helmProjectName
+    writeFile file: "${helmProjectName}/templates/${helmProjectName}-${templateCible}", text: contenuTemplateYaml
     index++
     }
   }
