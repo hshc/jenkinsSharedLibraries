1cc5559bc0e7878dadcde524e76dba223bc4442c
imqo007 <fpaillard@si2m.fr>
2020-04-14 12:41:17 +0200

    no message

--- vars/usilKubeInitProject.groovy
+++ vars/usilKubeInitProject.groovy
@@ -112,9 +112,9 @@ def roleBinding (def trigrammeAppli) {
 // - name: "ACC"
 //   active: true
 
-def serviceAccount1 (def trigrammeAppli) {
+def serviceAccount1 (string trigrammeAppli) {
 
-    def configYaml = '''\
+    string configYaml = '''\
     ---
     apiVersion: v1
     kind: ServiceAccount
