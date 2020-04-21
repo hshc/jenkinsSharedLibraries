6c96a0b3f755ee3fbdcc3b1e32963aa4731613cc
imqo007 <fpaillard@si2m.fr>
2020-04-21 11:22:22 +0200

    no message

--- vars/usilKubeInitProject.groovy
+++ vars/usilKubeInitProject.groovy
@@ -133,8 +133,8 @@ def serviceAccount1 (def trigrammeAppli) {
         echo "Le fichier serviceAccount.yaml n'existe pas, à créer"
     }
     def yamlFile = new File("serviceAccount.yaml")
-    def configYamlTRI = configYaml.replaceAll("trigrammeAppli","${trigrammeAppli}"
-    echo configYamlTRI
-    yamlFile.write(configYamlTRI))
+    def configYamlTRI = configYaml.replaceAll("trigrammeAppli","${trigrammeAppli}")
+    echo "${configYamlTRI}"
+    yamlFile.write(configYamlTRI)
 
 }
