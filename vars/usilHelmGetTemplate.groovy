aeed3340fc81d638254888a1b2d580d0a3117fb8
si2m_gqv <smurer@si2m.fr>
2020-04-23 17:32:32 +0200

    no message

--- vars/usilHelmGetTemplate.groovy
+++ vars/usilHelmGetTemplate.groovy
@@ -4,7 +4,7 @@ def call(def chartTemplateName,def gitBitbucketUrl) {
       sh ("rm -f -r ${chartTemplateName}")
       // récupération du modèle chart Helm sur le repo GIT et stockage dans le dossier avec le nom du modèle 
       checkout([$class: 'GitSCM', branches: [[name: '*/master']], 
-      doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${chartTemplateName}"]], submoduleCfg: [], userRemoteConfigs: 
+      doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout'],[$class: 'RelativeTargetDirectory', relativeTargetDir: "${chartTemplateName}"]], submoduleCfg: [], userRemoteConfigs: 
       [[credentialsId: 'jenkins-bitbucket-common-creds', url: "${gitBitbucketUrl}${chartTemplateName}"]]])
     }
 }
