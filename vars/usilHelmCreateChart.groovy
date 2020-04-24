340182c42ad9552d66363eb83dc0347b05900230
si2m_gqv <smurer@si2m.fr>
2020-04-24 12:07:34 +0200

    validation

--- vars/usilHelmCreateChart.groovy
+++ vars/usilHelmCreateChart.groovy
@@ -1,4 +1,4 @@
-def call(def chartTemplateName,def helmServiceName) {
+def call(def chartTemplateName,def helmServiceName, def kubServiceName) {
 stage("Create Chart Helm : ${helmServiceName} template: ${chartTemplateName}"){
   println("helmServiceName : ${helmServiceName}")
   // on liste les fichiers présent dans le repertoire ${chartTemplateName}/templates
@@ -6,7 +6,6 @@ stage("Create Chart Helm : ${helmServiceName} template: ${chartTemplateName}"){
   // on créé une liste contenant la liste des fichiers avec leurs paths
   println("findTemplate : ${findTemplate}")
   def listeFichierTemplate = findTemplate.readLines()
-  println "listeFichierTemplate : ${listeFichierTemplate}"
   // la fonction eachWithIndex nétant pas disponible une variable d'index est instancié 
   def index=0
   // fonctionne qui par la liste des fichiers
@@ -15,12 +14,11 @@ stage("Create Chart Helm : ${helmServiceName} template: ${chartTemplateName}"){
     contenuTemplateYaml = readFile it
     // on personnalise le contenu des variables du template modelTemplate par le nom du service (helmServiceName)
     contenuTemplateYaml = contenuTemplateYaml.replaceAll( 'modelTemplate', helmServiceName )
-    println listeFichierTemplate
     // on supprime le path 
     def templateCible=listeFichierTemplate[index].replaceAll("${chartTemplateName}/templates/",'')
     println "Préparation pour le service ${helmServiceName} du composant Helm : ${helmServiceName}_${templateCible}"
     // on écrit le fichier dans le repertoire helmServiceName/templates/ et on préfixe les fichiers avec helmServiceName
-    writeFile file: "${helmServiceName}/templates/${helmServiceName}-${templateCible}", text: contenuTemplateYaml
+    writeFile file: "${kubServiceName}/templates/${helmServiceName}-${templateCible}", text: contenuTemplateYaml
     index++
     }
   }
