08080c6844ec78dd77c795b2be60167760e3ff37
imqo007 <francis.paillard@malakoffhumanis.com>
2020-05-29 09:16:19 +0200

    renommer checkProd en checkDeployment

---
+++ vars/usilCheckDeployment.groovy
@@ -0,0 +1,62 @@
+def call(def codeEnv) { 
+    stage('Validation du déploiement en ' + "${codeEnv}") {
+        usilColorLog("stage", "Validation du déploiement en ${codeEnv}")
+        switch("${codeEnv}") { 
+            case "e3": 
+                ENV_TO_DEPLOY = "intg"
+                ENV_TO_TEST = "dev"
+                CODE_ENV_TO_TEST = "e4"
+                usilColorLog("warning", "Pas de validation, déploiement autorisé pour le moment pour cet environnement")
+                retrun true; 
+                break;
+            case "e2": 
+                ENV_TO_DEPLOY = "recf"
+                ENV_TO_TEST = "intg"
+                CODE_ENV_TO_TEST = "e3";
+                usilColorLog("warning", "Pas de validation, déploiement autorisé pour le moment pour cet environnement")
+                retrun true;
+                break;
+            case "el": 
+                ENV_TO_DEPLOY = "rec4"
+                ENV_TO_TEST = "recf"
+                CODE_ENV_TO_TEST = "e2"; 
+                usilColorLog("error", "Validation éffectuée, déploiement non autorisé pour cet environnement")
+                currentBuild.result = 'FAILURE'
+                sh "exit 1"
+                retrun false;
+                break;
+            case "er": 
+                ENV_TO_DEPLOY = "int2"
+                ENV_TO_TEST = "recf"
+                CODE_ENV_TO_TEST = "e2"; 
+                usilColorLog("error", "Validation éffectuée, déploiement non autorisé pour cet environnement")
+                currentBuild.result = 'FAILURE'
+                sh "exit 1"
+                retrun false;
+                break;
+            case "e1": 
+                ENV_TO_DEPLOY = "pprod"
+                ENV_TO_TEST = "recf"
+                CODE_ENV_TO_TEST = "e2"; 
+                usilColorLog("error", "Validation éffectuée, déploiement non autorisé pour cet environnement")
+                currentBuild.result = 'FAILURE'
+                sh "exit 1"
+                retrun false;
+                break;
+            case "e0": 
+                ENV_TO_DEPLOY = "prod"
+                ENV_TO_TEST = "pprod"
+                CODE_ENV_TO_TEST = "e1";
+                usilColorLog("error", "Validation éffectuée, déploiement non autorisé pour cet environnement")
+                currentBuild.result = 'FAILURE'
+                sh "exit 1"
+                retrun false;
+                break;
+            default:
+                usilColorLog("error", "Validation éffectuée, déploiement non autorisé pour cet environnement inconnu de l\'USIL3")
+                currentBuild.result = 'FAILURE'
+                sh "exit 1"
+                retrun false;
+       } 
+    }
+}
