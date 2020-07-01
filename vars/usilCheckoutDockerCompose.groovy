4a7e4cc31ede88dd030e673f2d83ded1c83f2fd4
Francis Paillard <francis.paillard@malakoffhumanis.com>
2020-07-01 15:08:17 +0000

    usilCheckoutDockerCompose.groovy edited online with Bitbucket

--- vars/usilCheckoutDockerCompose.groovy
+++ vars/usilCheckoutDockerCompose.groovy
@@ -4,7 +4,7 @@ def call(def codeEnv) {
                   doGenerateSubmoduleConfigurations: false, 
                   extensions: [[$class: 'CleanBeforeCheckout'],[$class: 'RelativeTargetDirectory', relativeTargetDir: "${codeEnv}"]],
                   submoduleCfg: [], 
-                  userRemoteConfigs: [[credentialsId: 'dev-jenkins', url: "https://gitlab.si2m.tec/admin/projects/usil/usil-dc.git", refspec: "+refs/heads/master:refs/remotes/origin/master"]]
+                  userRemoteConfigs: [[credentialsId: 'dev-jenkins', url: "https://gitlab.si2m.tec/admin/projects/usil/usil-dc.git"]]
                   //userRemoteConfigs: [[credentialsId: 'jenkins-bitbucket-common-creds', url: "${gitBitbucketUrl}${gitProjectDico}", refspec: "+refs/heads/master:refs/remotes/origin/master"]]])
         ])
 }
