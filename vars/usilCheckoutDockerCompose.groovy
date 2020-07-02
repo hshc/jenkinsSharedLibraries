4e80b2d0591545baf6fffa1e25148c48b2570e22
Francis Paillard <francis.paillard@malakoffhumanis.com>
2020-07-02 08:44:09 +0000

    usilCheckoutDockerCompose.groovy edited online with Bitbucket

--- vars/usilCheckoutDockerCompose.groovy
+++ vars/usilCheckoutDockerCompose.groovy
@@ -1,10 +1,17 @@
 def call(def codeEnv) {
-      checkout([$class: 'GitSCM', 
-                  branches: [[name: "*/master"]],
-                  doGenerateSubmoduleConfigurations: false, 
-                  extensions: [[$class: 'CleanBeforeCheckout'],[$class: 'RelativeTargetDirectory', relativeTargetDir: "${codeEnv}"]],
-                  submoduleCfg: [], 
-                  userRemoteConfigs: [[credentialsId: 'dev-jenkins', url: "https://gitlab.si2m.tec/usil/usil-dc.git"]]
+      //checkout([$class: 'GitSCM', 
+                  //branches: [[name: "*/master"]],
+                  //doGenerateSubmoduleConfigurations: false, 
+                  //extensions: [[$class: 'CleanBeforeCheckout'],[$class: 'RelativeTargetDirectory', relativeTargetDir: "${codeEnv}"]],
+                  //submoduleCfg: [], 
+                  //userRemoteConfigs: [[credentialsId: 'dev-jenkins', url: "https://gitlab.si2m.tec/usil/usil-dc.git"]]
                   //userRemoteConfigs: [[credentialsId: 'jenkins-bitbucket-common-creds', url: "${gitBitbucketUrl}${gitProjectDico}", refspec: "+refs/heads/master:refs/remotes/origin/master"]]])
+                  
+      checkout([$class: 'GitSCM', 
+                  branches: [[name: '*/master']], 
+                  doGenerateSubmoduleConfigurations: false, 
+                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'e3']], 
+                  submoduleCfg: [], 
+                  userRemoteConfigs: [[credentialsId: 'Gitlab', url: 'ssh://git@gitlab.si2m.tec:8022/usil/usil-dc.git']]])
         ])
 }
