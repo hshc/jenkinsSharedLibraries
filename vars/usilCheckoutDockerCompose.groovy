bd335364445f6c4332642263228dcc59be97470a
Francis Paillard <francis.paillard@malakoffhumanis.com>
2020-07-02 08:51:13 +0000

    usilCheckoutDockerCompose.groovy edited online with Bitbucket

--- vars/usilCheckoutDockerCompose.groovy
+++ vars/usilCheckoutDockerCompose.groovy
@@ -1,5 +1,7 @@
 def call(def codeEnv) {
-      //checkout([$class: 'GitSCM', 
+      stage("Récupération Gitlab env:${codeEnv} fichier: docker-compose-generique.yaml"){
+                  usilColorLog("stage", "Récupération Gitlab env:${codeEnv} fichier: docker-compose-generique.yaml")
+                  //checkout([$class: 'GitSCM', 
                   //branches: [[name: "*/master"]],
                   //doGenerateSubmoduleConfigurations: false, 
                   //extensions: [[$class: 'CleanBeforeCheckout'],[$class: 'RelativeTargetDirectory', relativeTargetDir: "${codeEnv}"]],
@@ -7,11 +9,11 @@ def call(def codeEnv) {
                   //userRemoteConfigs: [[credentialsId: 'dev-jenkins', url: "https://gitlab.si2m.tec/usil/usil-dc.git"]]
                   //userRemoteConfigs: [[credentialsId: 'jenkins-bitbucket-common-creds', url: "${gitBitbucketUrl}${gitProjectDico}", refspec: "+refs/heads/master:refs/remotes/origin/master"]]])
                   
-      checkout([$class: 'GitSCM', 
-                  branches: [[name: '*/master']], 
-                  doGenerateSubmoduleConfigurations: false, 
-                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'e3']], 
-                  submoduleCfg: [], 
-                  userRemoteConfigs: [[credentialsId: 'Gitlab', url: 'ssh://git@gitlab.si2m.tec:8022/usil/usil-dc.git']]])
-        ])
+                  checkout([$class: 'GitSCM', 
+                              branches: [[name: '*/master']], 
+                              doGenerateSubmoduleConfigurations: false, 
+                              extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'e3']], 
+                              submoduleCfg: [], 
+                              userRemoteConfigs: [[credentialsId: 'Gitlab', url: 'ssh://git@gitlab.si2m.tec:8022/usil/usil-dc.git']]])
+        ])}
 }
